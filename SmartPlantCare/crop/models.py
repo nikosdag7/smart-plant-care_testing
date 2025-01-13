2e3cad19c01d174caf7d5b754738333396bce50a
Grigoris Markakis <mtp329@edu.hmu.gr>
2025-01-13 22:53:31 +0200

    Update database Schema, minor fix

--- SmartPlantCare/crop/models.py
+++ SmartPlantCare/crop/models.py
@@ -50,7 +50,7 @@ class AreaName(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     area_id = db.Column(db.Integer, db.ForeignKey('area.id'), nullable=False) 
     language_id = db.Column(db.Integer, db.ForeignKey('language.id'), nullable=False)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return f"{self.id}:{self.area_id}:{self.language_id}:{self.name}"
